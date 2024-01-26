ce302c2c007da76b9fda0744d4d3b73729843732
ashpatx <thingslearn3@gmail.com>
2024-01-26 17:57:17 -0500

    Update script.js

--- JavaScript Algorithms and Data Structures/Project 3: Telephone Number Validator/script.js
+++ JavaScript Algorithms and Data Structures/Project 3: Telephone Number Validator/script.js
@@ -1,43 +1,40 @@
 const phoneCheck = (str) => {
-    const validPatterns = [
-      //1 555-555-5555 Valid! 
-      /^1 \d{3}-\d{3}-\d{4}$/,
-        
-      //1 (555) 555-5555 Valid!
-      /^1 \(\d{3}\) \d{3}-\d{4}$/,
-  
-      //5555555555 Valid!
-      /^\d{10}$/,
-  
-      //555-555-5555 Valid!
-      /^\d{3}-\d{3}-\d{4}$/,
-  
-      //(555)555-5555 Valid!
-      /\(\d{3}\)\d{3}-\d{4}$/,
-  
-      // (555)555-5555 Valid!
-      /^\(\d{3}\)\d{3}-\d{4}$/,
-  
-      //1(555)555-5555 Valid!
-      /^1\(\d{3}\)\d{3}-\d{4}/,
-  
-      //1 555 555 5555 Valid!
-      /^1 \d{3} \d{3} \d{4}$/
-    ];
-  
-    const userInput = document.getElementById("user-input").value;
-    const resultDiv = document.getElementById("results-div");
-  
-    if (userInput === "") {
-    alert("Please provide a phone number.");
-  } else if (validPatterns.some((pattern) => pattern.test(userInput))) {
-    resultDiv.textContent = `Valid US number: ${userInput}`;
-  } else {
-    resultDiv.textContent = `Invalid US number: ${userInput}`;
-  }
+  const validPatterns = [
+    //1 555-555-5555 Valid! 
+    /^1 \d{3}-\d{3}-\d{4}$/,
+      
+    //1 (555) 555-5555 Valid!
+    /^1 \(\d{3}\) \d{3}-\d{4}$/,
+
+    //5555555555 Valid!
+    /^\d{10}$/,
+
+    //555-555-5555 Valid!
+    /^\d{3}-\d{3}-\d{4}$/,
+
+    // (555)555-5555 Valid!
+    /^\(\d{3}\)\d{3}-\d{4}$/,  
+
+    //1(555)555-5555 Valid!
+    /1\(\d{3}\)\d{3}-\d{4}/,
+
+    //1 555 555 5555 Valid!
+    /^1 \d{3} \d{3} \d{4}$/
+  ];
+
+  const userInput = document.getElementById("user-input").value;
+  const resultDiv = document.getElementById("results-div");
+
+  if (userInput === "") {
+  alert("Please provide a phone number.");
+} else if (validPatterns.some((pattern) => pattern.test(userInput))) {
+  resultDiv.textContent = `Valid US number: ${userInput}`;
+} else {
+  resultDiv.textContent = `Invalid US number: ${userInput}`;
+}
 };
-  
-  const clearNumber = () => {
-    document.getElementById("user-input").value = "";
-    document.getElementById("results-div").textContent = "";
-  };
+
+const clearNumber = () => {
+  document.getElementById("user-input").value = "";
+  document.getElementById("results-div").textContent = "";
+};
