b1d1eee60e667c20a40e067a43b810f4e604a20f
ashpatx <thingslearn3@gmail.com>
2024-02-02 21:43:00 -0500

    Update script.js

--- JavaScript Algorithms and Data Structures/Project 5: Pokemon Search App/script.js
+++ JavaScript Algorithms and Data Structures/Project 5: Pokemon Search App/script.js
@@ -9,7 +9,6 @@ const hp = document.getElementById('hp');
 const types = document.getElementById('types');
 const attack = document.getElementById('attack');
 const defense = document.getElementById('defense');
-const attack = document.getElementById('attack');
 const specialAttack = document.getElementById('special-attack');
 const specialDefense = document.getElementById('special-defense');      
 const speed = document.getElementById('speed'); 
