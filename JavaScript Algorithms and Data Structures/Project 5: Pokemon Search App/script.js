cbc0371da3b7c3346c928c65fcb08e2af468b15f
ashpatx <thingslearn3@gmail.com>
2024-02-02 21:36:11 -0500

    Create script.js

---
+++ JavaScript Algorithms and Data Structures/Project 5: Pokemon Search App/script.js
@@ -0,0 +1,15 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-button');
+const pokemonContainer = document.getElementById('pokemon-container');
+//API details
+const pokemonId = document.getElementById('pokemon-name');
+const weight = document.getElementById('weight');
+const height = document.getElementById('height');
+const hp = document.getElementById('hp');
+const types = document.getElementById('types');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const attack = document.getElementById('attack');
+const special-attack = document.getElementById('special-attack');
+const special-defense = document.getElementById('special-defense');      
+const speed = document.getElementById('speed'); 
